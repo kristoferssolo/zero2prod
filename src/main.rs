77a2dd93891eeaf57db6ac571ef3c0cfecd7d0a6
Kristofers Solo <dev@kristofers.xyz>
2024-03-19 10:43:33 +0200

    feat: add basic routing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::SocketAddr;
+
+use axum::{extract::Path, http::StatusCode, response::IntoResponse, routing::get, Router};
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+    let router = Router::new()
+        .route("/", get(root))
+        .route("/:name", get(greet))
+        .route("/health_check", get(health_check));
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8000));
+    let listener = TcpListener::bind(addr).await.unwrap();
+    axum::serve(listener, router).await.unwrap();
+}
+async fn root() -> impl IntoResponse {
+    "Hello, world!"
+}
+
+async fn greet(Path(name): Path<String>) -> impl IntoResponse {
+    format!("Hello, {name}!")
+}
+async fn health_check() -> impl IntoResponse {
+    StatusCode::OK
 }
