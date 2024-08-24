0ba5f0c77d82cc4c82c18ae02809f941d7bd4930
Kristofers Solo <dev@kristofers.xyz>
2024-08-24 19:44:07 +0300

    feat: add subscibtion status

--- migrations/20240323135723_create_subscribtions_table.sql
+++ migrations/20240323135723_create_subscribtions_table.sql
@@ -1,7 +1,8 @@
 -- Add migration script here
 CREATE TABLE SUBSCRIPTIONS (
-  "id" UUID PRIMARY KEY NOT NULL,
-  "email" TEXT NOT NULL UNIQUE,
-  "name" TEXT NOT NULL,
-  "subscribed_at" TIMESTAMPTZ NOT NULL
+    "id" uuid PRIMARY KEY NOT NULL,
+    "email" text NOT NULL UNIQUE,
+    "name" text NOT NULL,
+    "subscribed_at" timestamptz NOT NULL
 );
+
